94baec8ea8b7d466c371b00b83ac9620158bc499
borsym <borsymatee@gmail.com>
2023-01-08 17:04:32 +0100

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoutes from './routes/auth.js';
+import usersRoutes from './routes/users.js';
+import hotelsRoutes from './routes/hotels.js';
+import roomsRoutes from './routes/rooms.js';
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('connectoed to mongodb');
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('mongodb disconnected');
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('mongodb connected');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// middleware
+app.use('api/auth', authRoutes);
+app.use('api/user', usersRoutes);
+app.use('api/hotels', hotelsRoutes);
+app.use('api/rooms', roomsRoutes);
+
+app.listen(8800, () => {
+  connect();
+  console.log('Server is running on port 8800');
+});
